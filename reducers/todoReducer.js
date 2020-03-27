215655a0aa9e8122f0eba3d69b5eebb4a61d51eb
shodayuki <yshoda1227@gmail.com>
2020-03-27 22:50:44 +0900

    Added todoReducer.js

---
+++ reducers/todoReducer.js
@@ -0,0 +1,46 @@
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  TOGGLE_TODO_COMPLETED
+} from '../../models.Todo';
+import Todo from '../models/Todo';
+
+/**
+ *
+ *  @param {Array<Todo>} state
+ *  @param {Object} action
+ *
+ */
+
+ export const todoReducer = (state = [], action) => {
+    switch (action.type) {
+      case ADD_TODO:
+        const todo = new Todo(action.text);
+        return [
+          ...state,
+          todo
+        ];
+
+      case DELETE_TODO:
+        return state.filter((todo, index) => {
+          return action.index !== index;
+        });
+
+      case TOGGLE_TODO_COMPLETED:
+        return state.map((todo, index) => {
+          if (action.index !== index) {
+            return todo;
+          }
+
+          const targetTodo = new Todo(todo.text);
+          if ( !todo.hasCompleted() ) {
+            targetTodo.Todo.toggle();
+          }
+
+          return targetTodo;
+        });
+
+      default:
+        return state;
+    }
+ }
